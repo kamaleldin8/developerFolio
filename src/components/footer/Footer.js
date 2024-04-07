0858b4ac876cd1c5141d2fe5c2978bf3aab9dc82
Kamal Mostafa <kamal.mostafa@nweave.com>
2024-04-08 00:01:28 +0200

    fix problem of deploy

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,17 +8,17 @@ export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      {/* <div className="footer-div">
+      <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made with ❤️")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+          {/* Theme by{" "} */}
           <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+            {/* developerFolio */}
           </a>
         </p>
-      </div> */}
+      </div>
     </Fade>
   );
 }
